735ac76473642116cfb845f0a8300c7fa3854269
Birat Poudel <poudel.birat25@gmail.com>
2022-01-14 00:27:30 +0545

    aboutus page updates

---
+++ js/aboutus.js
@@ -0,0 +1,65 @@
+//For Navigation Bar
+var header = document.querySelector("header");
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+const hamBurger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav__links");
+const navLinks = document.querySelectorAll(".nav__links li");
+
+navLinks.forEach((link, index) => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        hamBurger.classList.toggle('toggle');
+        nav.classList.toggle('display-off');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`;
+            }
+        })
+        }
+    )
+})
+
+const navSlide = () => {
+  hamBurger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = "";
+          }
+          else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`;
+          }
+      })
+      hamBurger.classList.toggle('toggle');
+      nav.classList.toggle('display-off');
+  })
+}
+
+navSlide();
+
+//For Show More and Show Less
+const abContainerExtra = document.querySelector('.ab__container__extra');
+const abContainerText = document.querySelector('#abc__text');
+const abContainerIcon = document.querySelector('img#abc__icon');
+const abContainerBody = document.querySelector('.ab__container__body');
+
+abContainerExtra.addEventListener('click', (e)=>{
+    abContainerBody.classList.toggle('show__more');
+    
+    if (abContainerText.innerText === "Show More"){
+        abContainerText.innerText = "Show Less";
+        abContainerIcon.style.transform = 'rotate(180deg)';
+    }
+    else{
+        abContainerText.innerText = "Show More"; 
+        abContainerIcon.style.transform = 'rotate(0deg)';
+    }
+})
